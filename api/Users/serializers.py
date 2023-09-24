7314e63579197594fcdcbb07fe80b5afb8f31073
m0shanya <funnytimka@gmail.com>
2023-09-24 20:19:55 +0300

    You can set password to user now;

--- api/Users/serializers.py
+++ api/Users/serializers.py
@@ -3,14 +3,24 @@ from rest_framework import serializers
 
 
 class UserModelSerializer(serializers.ModelSerializer):
-    # password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
         fields = (
             "id",
             "email",
-            "username"
+            "username",
+            "password"
         )
         read_only_fields = ("id",)
-        # extra_kwargs = {'password': {'write_only': True, 'min_length': 3}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 3}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data["email"],
+            username=validated_data["username"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
